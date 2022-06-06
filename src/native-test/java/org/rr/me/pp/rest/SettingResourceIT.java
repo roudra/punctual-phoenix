fac3c9285745b8f68dfa82b82d337fb53914761a
Roudranil Roy <bluesun.ray@gmail.com>
2022-06-06 17:49:28 +0530

    Integration Test Added

---
+++ src/native-test/java/org/rr/me/pp/rest/SettingResourceIT.java
@@ -0,0 +1,9 @@
+package org.rr.me.pp.rest;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class SettingResourceIT extends SettingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
