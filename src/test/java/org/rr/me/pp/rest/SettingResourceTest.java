d2c6fa76f7f705db942515bae1ab8c366e1c6adb
Roudranil Roy <bluesun.ray@gmail.com>
2022-06-06 17:48:32 +0530

    Sonar Code Smell Fix

--- src/test/java/org/rr/me/pp/rest/SettingResourceTest.java
+++ src/test/java/org/rr/me/pp/rest/SettingResourceTest.java
@@ -6,10 +6,10 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 
 @QuarkusTest
-public class SettingResourceTest {
+class SettingResourceTest {
 
     @Test
-    public void testHelloEndpoint() {
+    void testHelloEndpoint() {
         given()
                 .when().get("/settings")
                 .then()
