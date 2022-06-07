f6272c8c9bfb4cd64b13cb6cb230b56192284400
Roudranil Roy <bluesun.ray@gmail.com>
2022-06-07 20:17:15 +0530

    Integration Test Added

---
+++ src/native-test/java/org/rr/me/pp/it/ComprehensiveIT.java
@@ -0,0 +1,9 @@
+package org.rr.me.pp.it;
+
+import io.quarkus.test.junit.NativeImageTest;
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+import org.rr.me.pp.ComprehensiveTest;
+
+@QuarkusIntegrationTest
+public class ComprehensiveIT extends ComprehensiveTest {
+}
