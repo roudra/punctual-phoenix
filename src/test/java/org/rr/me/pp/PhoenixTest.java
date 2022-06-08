9d3e2b1f0cab6c91f6b82b063b1fccad627fc6dc
Roudranil Roy <bluesun.ray@gmail.com>
2022-06-08 23:19:59 +0530

    Add Feature Runner

--- src/test/java/org/rr/me/pp/PhoenixTest.java
+++ src/test/java/org/rr/me/pp/PhoenixTest.java
@@ -14,22 +14,13 @@ public class PhoenixTest {
 
     @Test
      void fullTest() {
-        Results results = Runner.path("classpath:org/rr/me/pp").tags("@smoke").parallel(4);
+        Results results = Runner.path("classpath:org/rr/me/pp").parallel(4);
         Assertions.assertEquals(results.getFailCount(), 0, results.getErrorMessages());
     }
 
     @Test
     void smokeTest() {
-        Results results = Runner.path("classpath:org/rr/me/pp").tags("@smoke").parallel(4);
+        Results results = Runner.path("classpath:org/rr/me/pp").tags("@smoke").parallel(1);
         Assertions.assertEquals(results.getFailCount(), 0, results.getErrorMessages());
     }
 }
-
-//    @Test
-//    void testHelloEndpoint() {
-//        given()
-//                .when().get("/hello")
-//                .then()
-//                .statusCode(200)
-//                .body(is("Hello from RESTEasy Reactive"));
-//    }
